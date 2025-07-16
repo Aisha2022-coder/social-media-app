cd4427601d8022deaf87851b3e312076c937e414
Aisha2022-coder <aisha.ghosh.cs.2022@mitmeerut.ac.in>
2025-07-17 01:06:25 +0530

    Initial commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'social-media-app-9f4t.onrender.com',
+      // add other domains if needed
+    ],
+  },
 };
 
 export default nextConfig;
